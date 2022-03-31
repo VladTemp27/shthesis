bb195073900563ca5cbb196f9c0b4cd7d072f179
VladTemp27 <mlestat2004@gmail.com>
2022-03-31 20:19:06 +0800

    Initial commit

---
+++ scihi_site/scihi_site/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+
+#for rendering templates without passing data
+def index(request):
+    return render(request, 'index.html')
+
+#for rendering templates for passing data
+def index2(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render({}, request))
+
+def about(request):
+    return render(request, "about.html")
+
+def portal(request):
+    return render(request, "portal.html")
