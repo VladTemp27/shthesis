76d127932a72f7b96076303d2c0928b52ff1428b
VladTemp27 <mlestat2004@gmail.com>
2022-05-13 22:07:10 +0800

    +

--- scihi_site/scihi_site/settings.py
+++ scihi_site/scihi_site/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'scihi_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '@HonoratoVlad',
+        'HOST': 'db.cdgpembbwtvgcfeoqjgw.supabase.co',
+        'PORT': '5432',
     }
 }
 
